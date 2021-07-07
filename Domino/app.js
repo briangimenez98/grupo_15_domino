5ad9e714bc3978a4b3d5f55acab56edf1a8e5540
TechnoAwesome <technoawesomexd@gmail.com>
2021-07-07 15:14:32 -0300

    first login

--- Domino/app.js
+++ Domino/app.js
@@ -5,6 +5,7 @@ const port = 3030;
 app.use(express.static('public'));
 
 app.get('/',(req,res)=> res.sendFile(path.join(__dirname, 'views' ,'home.html')));
+app.get('/login',(req,res)=> res.sendFile(path.join(__dirname, 'views' , 'login.html')))
 
-app.listen(port, ()=> console.log(`Server running in port ${port}`));
+app.listen(port, ()=> console.log(`Server running in http://localhost:${port}`));
 
