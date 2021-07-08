70529ceeb668c07350e0adefaecd82cba237149c
Martin <martin14.09.2002@gmail.com>
2021-07-08 19:46:56 -0300

    agregando ruta

--- Domino/app.js
+++ Domino/app.js
@@ -6,5 +6,6 @@ app.use(express.static('public'));
 
 app.get('/',(req,res)=> res.sendFile(path.join(__dirname, 'views' ,'home.html')));
 app.get('/detalle',(req,res) => res.sendFile(path.join(__dirname, 'views', 'detalle.html')));
+app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname, 'views', 'carrito.html')));
 app.listen(port, ()=> console.log(`Server running in port ${port}`));
 
