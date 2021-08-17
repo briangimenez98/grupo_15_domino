81cc66121d76c79abcb1eeda44f3db9d59f0c8b7
adrihui <huideadriana@gmail.com>
2021-08-17 12:58:06 -0300

    acomodo controller

--- Sitio/src/controllers/productsControllers.js
+++ Sitio/src/controllers/productsControllers.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const {productos, guardar} = require('../data/products_db');
+const { productos, guardar } = require('../data/products_db');
 const toThousand = require('../utils/toThousand');
 
 module.exports = {
@@ -11,61 +11,64 @@ module.exports = {
             toThousand,
         })
     },
-    detail : (req, res) => {
+    detail: (req, res) => {
         let producto = productos.find(p => p.id === +req.params.id)
         return res.render('detalle', {
             producto,
             productos
         })
     },
-    carrito : (req, res) => {
+    carrito: (req, res) => {
         res.render('carrito')
     },
-    createProduct : (req, res) => {
+    createProduct: (req, res) => {
         res.render('createProduct', {
-        productos,
+            productos,
         })
     },
-    addProduct : (req, res) => {
-        const {name, description,price,category, seccion, talle, clase} = req.body;
+    addProduct: (req, res) => {
+        const { name, description, price, category, seccion, talle, clase } = req.body;
 
-        let images= [(req.files[0]?req.files[0].filename:"defaultimage.png"),(req.files[1]?req.files[1].filename:"defaultimage.png"),(req.files[2]?req.files[2].filename:"defaultimage.png"),(req.files[3]?req.files[3].filename:"defaultimage.png")]
+        let images = [(req.files[0] ? req.files[0].filename : "defaultimage.png"), 
+                     (req.files[1] ? req.files[1].filename : "defaultimage.png"), 
+                     (req.files[2] ? req.files[2].filename : "defaultimage.png"), 
+                     (req.files[3] ? req.files[3].filename : "defaultimage.png")]
 
-            let producto = {
-                id : productos[productos.length - 1].id + 1,
-                seccion,
-                clase,
-                name,
-                price : +price,
-                image : images,
-                category,
-                talle,
-                description
-            }
-           productos.push(producto);
-           guardar(productos)
-           return res.redirect('/products/')
+        let producto = {
+            id: productos[productos.length - 1].id + 1,
+            seccion,
+            clase,
+            name,
+            price: +price,
+            image: images,
+            category,
+            talle,
+            description
+        }
+        productos.push(producto);
+        guardar(productos);
+        return res.redirect('/products/');
     },
     editProduct: (req, res) => {
         let producto = productos.find(p => p.id === +req.params.id)
-        return res.render('editProduct',{
+        return res.render('editProduct', {
             producto,
             productos
         })
     },
-    edit : (req, res) => {
+    edit: (req, res) => {
         res.send(req.body)
     },
-    destroy : (req, res) => {
+    destroy: (req, res) => {
 
-		productos.forEach(producto => {
-			if (producto.id === +req.params.id){
-				let productAEliminar = productos.indexOf(producto);
-				productos.splice(productAEliminar,1)
-			}
-		});
+        productos.forEach(producto => {
+            if (producto.id === +req.params.id) {
+                let productAEliminar = productos.indexOf(producto);
+                productos.splice(productAEliminar, 1)
+            }
+        });
 
         guardar(productos)
-		return res.redirect('/products/')
-	}
+        return res.redirect('/products/')
+    }
 }
