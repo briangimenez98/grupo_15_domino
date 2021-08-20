2d871621c25abb5e806143083ad18eae40bb2d60
adrihui <huideadriana@gmail.com>
2021-08-20 19:51:54 -0300

    implemento funcionalidad al register

--- Sitio/src/controllers/usersController.js
+++ Sitio/src/controllers/usersController.js
@@ -1,5 +1,6 @@
-const users = require('../data/users_db');
+const {usuarios, guardar} = require('../data/users_db')
 const {validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
     login : (req, res) => {
@@ -8,11 +9,23 @@ module.exports = {
     register : (req, res) => {
         res.render('register');
     },
-    registerValidator : (req, res) => {
+    processRegister : (req, res) => {
         let errors = validationResult(req);
-
+        let {nombre,apellido,email,password,birthday} = req.body;
         if(errors.isEmpty()){
-            res.redirect('/')
+
+            let usuario = {
+                id : usuarios.length > 0 ? usuarios[usuario.length - 1].id + 1 : 1,
+                nombre,
+                apellido,
+                email,
+                password : bcrypt.hashSync(password,10),
+                birthday,
+                rol : "user"
+            }
+            usuarios.push(usuario);
+            guardar(usuarios);
+            res.redirect('/');
         } else {
             res.render('register',{errors : errors.mapped(), old: req.body})
         };
--- Sitio/src/routes/users.js
+++ Sitio/src/routes/users.js
@@ -1,7 +1,7 @@
 /* Requires */
 const express = require('express');
 var router = express.Router();
-const {login, register, registerValidator, loginValidator} = require('../controllers/usersController')
+const {login, register, processRegister, loginValidator} = require('../controllers/usersController')
 const registerValidation = require('../validations/registerValidation');
 const loginValidation = require('../validations/loginValidation');
 
@@ -9,5 +9,5 @@ const loginValidation = require('../validations/loginValidation');
 router.get('/login', login);
 router.post('/login',loginValidation, loginValidator);
 router.get('/register', register);
-router.post('/register', registerValidation , registerValidator);
+router.post('/register', registerValidation , processRegister);
 module.exports = router;
