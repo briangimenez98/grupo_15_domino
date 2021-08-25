c645786379694b7c575f74733bf3898bf98d3d81
briangimenez98 <brian.gim98@gmail.com>
2021-08-25 19:50:45 -0300

    loginprocess,logout & rutahuespedes

--- Sitio/src/routes/users.js
+++ Sitio/src/routes/users.js
@@ -1,13 +1,14 @@
 /* Requires */
 const express = require('express');
 var router = express.Router();
-const {login, register, processRegister, loginValidator} = require('../controllers/usersController')
+const {login, register, processRegister, processLogin, logout} = require('../controllers/usersController')
 const registerValidation = require('../validations/registerValidation');
 const loginValidation = require('../validations/loginValidation');
 
 /* Routes */
 router.get('/login', login);
-router.post('/login',loginValidation, loginValidator);
+router.post('/login',loginValidation, processLogin);
 router.get('/register', register);
 router.post('/register', registerValidation , processRegister);
+router.get('/logout',logout);
 module.exports = router;
